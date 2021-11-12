db0922356002d269e6a41d52846964f9ebfcb5c9
bishopfox <h4ng3r@users.noreply.github.com>
2021-11-13 00:16:17 +0100

    Test

--- concurrency/job.go
+++ concurrency/job.go
@@ -1,15 +1,4 @@
 package concurrency
 
-import (
-	//"time"
-)
-
-type Job struct {
-    ID        int
-    //CreatedAt time.Time
-    //UpdatedAt time.Time
-    Input interface{}
-}
-
-type JobChannel chan Job
-type JobQueue chan chan Job
+type JobChannel chan interface{}
+type JobQueue chan chan interface{}
--- concurrency/worker.go
+++ concurrency/worker.go
@@ -1,29 +1,28 @@
 package concurrency
 
 import (
-    "sync"
+	"sync"
 )
 
 type Worker struct {
-    ID      int
-    JobChan JobChannel
-    Queue   JobQueue   // shared between all workers and dispatchers.
-    Quit    chan struct{}
+	ID      int
+	JobChan JobChannel
+	Queue   JobQueue // shared between all workers and dispatchers.
+	Quit    chan struct{}
 }
 
 func (wr *Worker) Start(wg *sync.WaitGroup, jobFunc JobFuncType) {
-    go func() {
-        for {
-            wr.Queue <- wr.JobChan
-            select {
-            case job := <-wr.JobChan:
-                // TODO: status struct
-            	jobFunc(job.Input, wr.ID, job.ID)
-                wg.Done()
-            case <-wr.Quit:
-                close(wr.JobChan)
-                return
-            }
-        }
-    }() 
+	go func() {
+		for {
+			wr.Queue <- wr.JobChan
+			select {
+			case job := <-wr.JobChan:
+				jobFunc(job, wr.ID)
+				wg.Done()
+			case <-wr.Quit:
+				close(wr.JobChan)
+				return
+			}
+		}
+	}()
 }
